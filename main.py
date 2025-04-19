b0ee5abfefa2abe571bb10046f06c212e464bd21
Nariman Emam <141886157+NarimanEmam@users.noreply.github.com>
2025-04-19 20:33:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from Anomly import Anomaly
-Machines = {
+import pandas as pd
+from Anomaly import Anomaly
+
+machines = {
     "1A0": "1A0.csv",
     "1B0": "1B0.csv",
     "1C0": "1C0.csv",
@@ -7,12 +9,11 @@ Machines = {
     "1E0": "1E0.csv"
 }
 
+anomaly_checker = Anomaly()
 
-
-
-if __name__ == "__main__":
-    anomaly_checker = Anomaly()
-    for id in Machines:
-        results = anomaly_checker.check_anomaly(id, Machines[id])
-    anomaly_checker.save_to_log(results)
-    print("Anomaly detection completed. Check anomaly_log.txt for results.")
+for device_id, file_path in machines.items():
+    results = anomaly_checker.check_anomaly(device_id, file_path)
+    if results:
+        anomaly_checker.save_to_log(device_id, results)
+    else:
+        print(f"No anomalies found for device {device_id}.")
